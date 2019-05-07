2530ee241c0f9254e13d6b95ae41ad5d73dd93b0
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-05-07 16:22:10 -0400

    <hw>

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+// Import dependencies 
+var express = require("express");
+
+var router = express.router();
+
+var burger = require("./burger.js");
+
+// Routes 
+router.get("/",)
+
+
+
+
+
+module.exports = router; 
---
+++ models/burger.js
@@ -0,0 +1,19 @@
+var orm = require("../orm");
+
+    var burgers = {
+
+        selectAll: function(cb){
+            orm.selectAll("burgers",)
+            },
+
+        insertOne: function(newEntry, cb){
+            orm.insertOne("burgers", );
+            },
+
+        updateOne: function(cb){
+            orm.updateOne("burgers",)
+            },
+
+    };
+
+module.exports = burgers; 
