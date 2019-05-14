0585db2bb092da11b452ee38dfe7b1dfb19c4f14
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-05-14 10:17:17 -0400

    <hw>

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -18,8 +18,6 @@ $(function() {
     });  
 
     $(".form").on("submit", function(event) {
-        // Make sure to preventDefault on a submit event.
-        // event.preventDefault();
 
         var name = $("[name=burger-name]").val().trim();
 
